239ee4ee7ea913fda5ee72fc344558429a8d609b
Alex Kalkatos <alexcsts@gmail.com>
2025-08-28 20:54:48 -0300

    Project structure overhaul

--- GodotBase/addons/GenkiAnalytics/Samples/AnalyticsTester.gd
+++ GodotBase/addons/GenkiAnalytics/Samples/AnalyticsTester.gd
@@ -1,9 +1,12 @@
 extends Node
 
 func _ready() -> void:
-	SignalBus._on_settings_panel.connect(_handle_settings_panel)
+	# Connect to signal
+	# SignalBus._on_settings_panel.connect(_handle_settings_panel)
+	pass
 
 
 func _handle_settings_panel (open: bool):
+	# Send event
 	if (open):
 		Analytics.send_event("settings_opened")
