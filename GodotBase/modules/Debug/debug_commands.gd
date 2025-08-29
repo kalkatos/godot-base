18d3a6f09835e1722bd2e2e15bacbf4aa403d082
Alex Kalkatos <alexcsts@gmail.com>
2025-08-29 20:34:33 -0300

    Debug commands actually execute functions

--- GodotBase/modules/Debug/debug_commands.gd
+++ GodotBase/modules/Debug/debug_commands.gd
@@ -5,6 +5,7 @@ extends CanvasLayer
 @export var input_text: LineEdit
 
 var is_open: bool = false
+var commands: Dictionary[String, Callable] = {}
 
 
 func _ready() -> void:
@@ -12,14 +13,11 @@ func _ready() -> void:
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_released("toggle_debug"):
+	if event.is_action_released("ui_cancel"):
 		is_open = !is_open
 		visible = is_open
-
-
-func _handle_text_submitted (text: String):
-	input_text.clear()
-	logm("Function executed: " + text)
+		if is_open:
+			input_text.grab_focus()
 
 
 func logm (msg: String):
@@ -31,10 +29,43 @@ func logm (msg: String):
 func log_warning (msg: String):
 	push_warning(msg)
 	debug_text.newline()
-	debug_text.append_text("[color=red]%s[/color]" % msg)
+	debug_text.append_text("[color=yellow]%s[/color]" % msg)
 
 
 func log_error (msg: String):
 	push_error(msg)
 	debug_text.newline()
-	debug_text.append_text("[color=yellow]%s[/color]" % msg)
+	debug_text.append_text("[color=red]%s[/color]" % msg)
+
+
+func add_function (func_name: String, callable: Callable) -> void:
+	commands[func_name] = callable
+
+
+func _handle_text_submitted (text: String):
+	input_text.clear()
+	var has_params = text.find(" ") != -1
+	if has_params:
+		var split = text.split(" ")
+		var func_name = split[0]
+		var params = split.slice(1, split.size())
+		if commands.has(func_name):
+			commands[func_name].callv(params)
+			logm("Function executed: " + func_name + " with params: " + str(params))
+		else:
+			log_warning("Command not found: " + func_name)
+	elif commands.has(text):
+		if commands[text].get_argument_count() > 0:
+			log_warning("Command requires parameters: " + text)
+		else:
+			commands[text].call()
+			logm("Function executed: " + text)
+	else:
+		log_warning("Command not found: " + text)
+	call_deferred("_grab_focus_back")
+
+
+func _grab_focus_back () -> void:
+	visible = false
+	visible = true
+	input_text.grab_focus()
