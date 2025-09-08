de4754b18fd7f4133baeef320ec8726391e357fc
Alex Kalkatos <alexcsts@gmail.com>
2025-09-07 22:14:00 -0300

    Updates for globals

--- GodotBase/Game/Globals/global.gd
+++ GodotBase/Game/Globals/global.gd
@@ -1,3 +1,13 @@
 extends Node
 ## Global project-wide constants and utility functions
 
+# Settings
+var highlight_height: float = 1.0
+var music_volume: float = 1.0
+var sfx_volume: float = 1.0
+var settings_panel_open: bool = false
+
+# Sorting orders
+var hand_card_sorting: int = 0
+var highlighted_card_sorting: int = 30
+var drag_card_sorting: int = 40
--- GodotBase/Game/Globals/signal_bus.gd
+++ GodotBase/Game/Globals/signal_bus.gd
@@ -1,28 +1,33 @@
 extends Node
+## All signals here must start with '_on_' to be auto-connected to SignalBus
 
-# All signals here must start with undescore '_'
+# ███████████████████████████████  S Y S T E M S  █████████████████████████████████
+
 signal _on_music_volume_set (value: float)
-signal _on_sfx_volume_set (value: float)
-signal _on_settings_panel (open: bool)
-
-
-func emit_on_settings_panel (open: bool):
+func emit_on_music_volume_set (value: float):
+	Global.music_volume = value
 	if SignalBus != self:
-		_on_settings_panel.emit(open)
-	SignalBus._on_settings_panel.emit(open)
+		_on_music_volume_set.emit(value)
+	SignalBus._on_music_volume_set.emit(value)
 
 
+signal _on_sfx_volume_set (value: float)
 func emit_on_sfx_volume_set (value: float):
+	Global.sfx_volume = value
 	if SignalBus != self:
 		_on_sfx_volume_set.emit(value)
 	SignalBus._on_sfx_volume_set.emit(value)
 
 
-func emit_on_music_volume_set (value: float):
+signal _on_settings_panel (open: bool)
+func emit_on_settings_panel (open: bool):
+	Global.settings_panel_open = open
 	if SignalBus != self:
-		_on_music_volume_set.emit(value)
-	SignalBus._on_music_volume_set.emit(value)
+		_on_settings_panel.emit(open)
+	SignalBus._on_settings_panel.emit(open)
 
+
+# ███████████████████████████████  R E A D Y  ███████████████████████████████████
 
 func _ready ():
 	if self == SignalBus:
