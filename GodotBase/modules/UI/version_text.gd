1a54e01099973d517b96dee9940cc989e60335b7
Alex Kalkatos <alexcsts@gmail.com>
2026-05-22 17:47:52 -0300

    Remove modules

--- GodotBase/modules/UI/version_text.gd
+++
@@ -1,7 +0,0 @@
-## Automatically updates the label text with the current application version from ProjectSettings.
-extends Label
-
-
-## Retrieves and displays the version string on initialization.
-func _ready () -> void:
-	set_text(ProjectSettings.get_setting("application/config/version"))
--- GodotBase/modules/Utils/particle_emitter.gd
+++
@@ -1,5 +0,0 @@
-class_name ParticleEmitter
-extends Resource
-
-@export var particle: NodePath
-@export var delay: float = 0.0
